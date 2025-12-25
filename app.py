ec08028a8893add68f639f6a7e0c51424e65b99d
wizard-wizard <kate.eliza@gmail.com>
2025-12-25 02:12:44 -0500

    Change app run configuration to use environment port

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ def generate():
 
 if __name__ == "__main__":
     # default: http://127.0.0.1:5000
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port, debug=True)
